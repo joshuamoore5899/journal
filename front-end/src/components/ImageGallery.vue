8bbdbf4acc78dd4072da5f73a969290a2ef5f6f8
joshuamoore5899 <joshuamoore5899@gmail.com>
2021-04-21 00:43:42 -0600

    first commit

---
+++ front-end/src/components/ImageGallery.vue
@@ -0,0 +1,87 @@
+<template>
+<div>
+  <section class="image-gallery">
+    <div class="image" v-for="photo in photos" v-bind:key="photo._id">
+      <img :src="photo.path" />
+      <p class="photoDate">{{formatDate(photo.created)}} was a(n) {{photo.title}} day</p>
+      <div class="photoInfo">
+        <p class="photoName">{{photo.description}}</p>
+      </div>
+    </div>
+  </section>
+</div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'ImageGallery',
+  props: {
+    photos: Array
+  },
+  methods: {
+    formatDate(date) {
+        return moment(date).format('MMMM DD, YYYY');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.photoInfo {
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.2em;
+}
+
+.photoInfo p {
+  margin: 20px;
+}
+
+.photoDate {
+  font-size: 1.0em;
+  font-weight: bold;
+}
+
+p {
+  margin: 0px;
+}
+
+/* Masonry */
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+.image-gallery {
+  column-gap: 1em;
+}
+
+.image {
+  flex-direction: column;
+  align-items: center;
+  margin: 0 0 1.5em;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+
+.image img {
+  width: 95%;
+}
+
+@media only screen and (min-width: 1024px) {
+  .image img {
+  width: 40%;
+  }
+}
+
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .image img {
+  width: 55%;
+}
+}
+</style>
---
+++ front-end/src/components/MyPhotos.vue
@@ -0,0 +1,80 @@
+<template>
+<div class="main">
+  <div class="menu">
+    <h2><a @click="toggleUpload">New Entry <i class="fas fa-book"></i></a></h2>
+    <h2>{{user.firstName}} {{user.lastName}} | Logout <a @click="logout"><i class="fas fa-sign-out-alt"></i></a></h2>
+    <uploader :show="show" @close="close" @uploadFinished="uploadFinished" />
+  </div>
+  <p v-if="error">{{error}}</p>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+import Uploader from '@/components/Uploader.vue';
+export default {
+  name: 'MyPhotos',
+  components: {
+    Uploader,
+  },
+  data() {
+    return {
+      show: false,
+      photos: [],
+      error: '',
+    }
+  },
+  created() {
+    this.getPhotos();
+  },
+  computed: {
+    user() {
+      return this.$root.$data.user;
+    },
+  },
+  methods: {
+    async logout() {
+      try {
+        await axios.delete("/api/users");
+        this.$root.$data.user = null;
+      } catch (error) {
+        this.$root.$data.user = null;
+      }
+    },
+    async getPhotos() {
+      try {
+        this.response = await axios.get("/api/photos");
+        this.photos = this.response.data;
+      } catch (error) {
+        this.error = error.response.data.message;
+      }
+    },
+    close() {
+      this.show = false;
+    },
+    toggleUpload() {
+      this.show = true;
+    },
+    async uploadFinished() {
+      this.show = false;
+      this.getPhotos();
+    },
+  }
+}
+</script>
+
+<style scoped>
+.menu {
+  display: flex;
+  justify-content: space-between;
+}
+
+.menu h2 {
+  font-size: 14px;
+}
+
+.main-content {
+  display: flex;
+  justify-content: center;
+}
+</style>
